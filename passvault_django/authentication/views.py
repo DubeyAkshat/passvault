fed4798eaa5a328647de0587c3b326e9d8d3e4d1
Akshat Dubey <iakshatdubey@gmail.com>
2023-09-06 03:59:59 +0530

    Created UserLoginView and UserLoginSerializer

--- passvault_django/authentication/views.py
+++ passvault_django/authentication/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializers import UserRegistrationSerializer
-
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from django.contrib.auth import authenticate
 
 class UserRegistrationView(APIView):
     def post(self, request, format=None):
@@ -10,4 +10,17 @@ class UserRegistrationView(APIView):
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
             return Response ({'msg':'Account Created'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                return Response({'msg':'Login Successful'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors':{'non_field_erors':['Incorrect Credentials']}}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

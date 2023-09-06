8fc6708ce0837ad34eff840aef25a67370c36fb7
Akshat Dubey <iakshatdubey@gmail.com>
2023-09-06 21:07:47 +0530

    Added custom JSON Renderer

--- passvault_django/authentication/views.py
+++ passvault_django/authentication/views.py
@@ -1,10 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from .models import User
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
+from .renderers import UserRenderer
+from .utils import normalize_email
 
 class UserRegistrationView(APIView):
+    renderer_classes = [UserRenderer]
     def post(self, request, format=None):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -13,14 +17,18 @@ class UserRegistrationView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class UserLoginView(APIView):
+    renderer_classes = [UserRenderer]
     def post(self, request, format=None):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            email = serializer.data.get('email')
+            normalized_email = normalize_email(serializer.data.get('email'))
             password = serializer.data.get('password')
-            user = authenticate(email=email, password=password)
+            user = authenticate(email=normalized_email, password=password)
             if user is not None:
-                return Response({'msg':'Login Successful'}, status=status.HTTP_200_OK)
+                if user.is_active:
+                    return Response({'msg':'Login Successful.'}, status=status.HTTP_200_OK)
+                else:
+                    return Response({'error':'The user is deactivated.'}, status=status.HTTP_403_FORBIDDEN)
             else:
-                return Response({'errors':{'non_field_erors':['Incorrect Credentials']}}, status=status.HTTP_404_NOT_FOUND)
+                return Response({'errors':{'non_field_erors':['Incorrect Credentials.']}}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
